3f44d83c249a09442cbf07f7c89f8471302b5af1
Gaurav <gauravsuhag7ds@gmail.com>
2024-11-10 14:23:42 +0900

    cursor and hr scroll section

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function raf(time) {
   requestAnimationFrame(raf)
 }
 
-requestAnimationFrame(raf)
+raf()
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100);
@@ -53,21 +53,13 @@ const tick = ()=>{
 tick()
 
 
-
-
-
-
-
-
-
-
+const cursor = document.querySelector('.cursor');
+const cursorText = document.querySelector('.cursor-inner p');
 
 
 
 
 gsap.registerPlugin(ScrollTrigger);
-
-//const navHeight = document.querySelector('header').getBoundingClientRect().height;
 
 const tl = gsap.timeline({
   defaults:{
@@ -130,10 +122,11 @@ projectsTl.to('.image1',{
 
 const projects = [...document.querySelectorAll('.project')];
 
+const images = [...document.querySelectorAll('.image')]
 
+let currentColor = '#000';
 projects.forEach((p, i) => {
   const color = p.dataset.color;
-
   gsap.to(p, {
     scrollTrigger: {
       trigger: p,
@@ -145,16 +138,35 @@ projects.forEach((p, i) => {
           duration: 1,
           ease: 'power4.out'
         });
+        currentColor = images[i].dataset.color
+        gsap.to(cursor,{
+          backgroundColor:images[i].dataset.color,
+          duration:.6, 
+          ease: 'power4.out'
+        })
       },
-      // onLeave: () => {
-      //   const nextColor = i < projects.length - 1 ? projects[i + 1].dataset.color : color;
-      //   gsap.to('body', {
-      //     backgroundColor: nextColor,
-      //     duration: 1,
-      //     ease: 'power4.out'
-      //   });
-      // },
+      onLeave: () => {
+        currentColor = '#000'
+        gsap.to(cursor,{
+          backgroundColor:'#000',
+          duration:.6, 
+          ease: 'power4.out'
+        })
+        if( i == projects.length - 1 ){
+          gsap.to('body', {
+            backgroundColor: '#fff',
+            duration: 1,
+            ease: 'power4.out'
+          });
+        }
+      },
       onEnterBack: () => {
+        currentColor = images[i].dataset.color
+        gsap.to(cursor,{
+          backgroundColor:images[i].dataset.color,
+          duration:.6, 
+          ease: 'power4.out'
+        })
         gsap.to('body', {
           backgroundColor: color,
           duration: 1,
@@ -162,6 +174,11 @@ projects.forEach((p, i) => {
         });
       },
       onLeaveBack: () => {
+        gsap.to(cursor,{
+          backgroundColor:'#000',
+          duration:.6, 
+          ease: 'power4.out'
+        })
         if(i==0){
           gsap.to('body', {
             backgroundColor: '#fff',
@@ -173,3 +190,89 @@ projects.forEach((p, i) => {
     }
   });
 });
+
+const hrWidth = document.querySelector('.hr-inner').getBoundingClientRect().width;
+
+gsap.to('.hr-inner', {
+  x:`${-hrWidth}px`,
+  scrollTrigger: {
+    trigger:'.hr-scroll',
+    start:'top top',
+    end:`+=${hrWidth}px`,
+    scrub:2,
+    pin:true,
+  }
+})
+
+const cardss = [...document.querySelectorAll('.card-hr')];
+
+cardss.forEach((card) => {
+  card.addEventListener('mousemove', (e) => {
+    const rect = card.getBoundingClientRect();
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    let x = e.clientX - rect.left - centerX;
+    let y = e.clientY - rect.top - centerY;
+    y = y*.2
+    x = x*.1
+    gsap.to(card,{
+      x:`${x}px`,
+      y:`${y}px`,
+      duration:.5,
+      ease:'power4.out' 
+    })
+    
+  })
+  card.addEventListener('mouseleave', () => {
+    gsap.to(card, {
+      x:0,
+      y:0,
+      duration:.7,
+      ease:'bounce.out'
+    })
+  })
+})
+images.forEach((img) => {
+  img.addEventListener('mouseenter', () => {
+    console.log('mouseenter');
+    
+    gsap.to('.cursor',{
+      width:'8rem',
+      height:'8rem',
+      backgroundColor:img.dataset.color,
+      duration:.6,
+      ease:'power4.out'
+    })
+    gsap.to(cursorText,{
+      opacity:1,
+      duration:.6,
+      ease:'power4.out'
+    })
+  })
+  img.addEventListener('mouseleave', () => {
+    gsap.to('.cursor',{
+      width:'1.3rem',
+      height:'1.3rem',
+      backgroundColor:currentColor,
+      duration:.6,
+      ease:'power4.out'
+    })
+    gsap.to(cursorText,{
+      opacity:0,
+      duration:.6,
+      ease:'power4.out'
+    })
+  })
+})
+
+
+
+
+document.addEventListener('mousemove', (e) => {
+  gsap.to(cursor, {
+    x: e.clientX,
+    y: e.clientY,
+    duration:.7,
+    ease:'power4.out'
+  })
+})
